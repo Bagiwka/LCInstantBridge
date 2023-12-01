6bc128d73075205fe0b66d414b3b42e00bb2933a
Bagiwka <sagivka2006@gmail.com>
2023-12-01 12:06:08 +0100

    instant bridge working

---
+++ LCInstantBridge/Patches/BridgeTriggerPatch.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace LCInstantBridge.Patches
+{
+    [HarmonyPatch(typeof(BridgeTrigger))]
+    [HarmonyPatch("Update")]
+    internal class BridgeTriggerUpdatePatch
+    {
+        private static void Postfix(BridgeTrigger __instance)
+        {
+            // Modify bridgeDurability as needed
+            __instance.bridgeDurability = Mathf.Clamp(__instance.bridgeDurability - Time.deltaTime * 0.1f, 0f, 1f)*0.001f;
+        }
+    }
+}
